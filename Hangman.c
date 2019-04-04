e41b95249eb09e07b9b877d3ebca8e338490445e
Expozz <46921586+Expozz@users.noreply.github.com>
2019-04-04 13:31:41 +0200

    Update Hangman.c

--- Hangman.c
+++ Hangman.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 int main() {
 
       srand(time(NULL));
@@ -12,14 +14,15 @@ int main() {
         "yes"
       };
 
-      int randomIndex = rand() % 3 ; //byt 0 mot antal ord
+
+      int randomIndex = rand() % 3 ; //ordets plats
 
       int numLives = 8;
       int numCorrect = 0; //antal rätt gissningar
       int oldCorrect = 0;
 
       int lenghtOfWord = strlen(guessWords[randomIndex]);
-      int letterGuessedPos [8] = { 0,0,0,0,0,0,0,0 };
+      int letterGuessedPos[8]={0,0,0,0,0,0,0,0};
 
       int quit = 0;
 
@@ -28,39 +31,76 @@ int main() {
       char guess[16];
       char letterGuessed;
 
-      printf("guessWords: %s randomIndex: %d lenghtOfWord: %d \n",
+      printf("Word: %s randomIndex: %d lenghtOfWord: %d \n",
       guessWords[randomIndex],
       randomIndex,
       lenghtOfWord);
 
-      while ( numCorrect < lenghtOfWord) {
+      while(numCorrect<lenghtOfWord){
+
+        printf("\n\nWord:");
+        for(loopIndex = 0; loopIndex < lenghtOfWord; loopIndex++){
+
+          if(letterGuessedPos[loopIndex] == 1){
+             printf("%c",guessWords[randomIndex][loopIndex]);
+        }
+          else{
+            printf("-");
+          }
+
+        }
+
+          printf("\n");
+
         printf("Number correct so far: %d\n",numCorrect );
+        sleep(1);
+
         printf("Guess a letter: ");
         fgets(guess, 16, stdin);
+        sleep(1);
 
-        if( strncmp(guess, "quit", 4) == 0) {
+        if(strncmp(guess, "quit", 4) == 0){
           quit = 1;
           break;
         }
 
         letterGuessed = guess[0];
 
-        printf("letterGuessed: %c\n", letterGuessed);
+        oldCorrect = numCorrect;
         for(loopIndex = 0; loopIndex < lenghtOfWord; loopIndex++){
 
-          if(letterGuessedPos[loopIndex] == 1) {
+          if(letterGuessedPos[loopIndex] == 1){
             continue;
-          }
+        }
 
-          if(letterGuessed == guessWords[randomIndex][loopIndex]) {
+          if(letterGuessed == guessWords[randomIndex][loopIndex]){
             letterGuessedPos[loopIndex] = 1; numCorrect++;
           }
         }
 
+          if(oldCorrect == numCorrect){
+            numLives--;
+
+
+
+            printf("Wrong.\nYou have %d lives left\n",numLives);
+            if(numLives == 0){
+              break;
+            }
+          }
+            else{
+            printf("Correct.\n");
+          }
+
       }
 
       if( quit == 1 ){
-        printf("\nAre you sure you want to quit?\nYes/No: ");
+        printf("\nYou quit.\n");
+      }
+        else if(numLives == 0){
+          printf("\nYou lost, the word was: %s\n", guessWords[randomIndex]);
+        }
+        else{printf("\n You won!\n");
       }
 
   return 0;
